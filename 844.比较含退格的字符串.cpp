b835152f2ae16b0c7494a3a5f03f2ea5d5494cec
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-09 14:56:54 +0800

    844 1st 0ms double pointers

---
+++ 844.比较含退格的字符串.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=844 lang=cpp
+ *
+ * [844] 比较含退格的字符串
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int back_s = 0, i_s = s.length() - 1;
+        int back_t = 0, i_t = t.length() - 1;
+        while (i_s >= 0 || i_t >= 0) {
+            while (i_s >= 0) {
+                if (s[i_s] == '#') {
+                    back_s++;
+                    i_s--;
+                } else if (back_s > 0) {
+                    back_s--;
+                    i_s--;
+                } else {
+                    break;
+                }
+            }
+            while (i_t >= 0) {
+                if (t[i_t] == '#') {
+                    back_t++;
+                    i_t--;
+                } else if (back_t > 0) {
+                    back_t--;
+                    i_t--;
+                } else {
+                    break;
+                }
+            }
+            if (i_s >= 0 && i_t >= 0) {
+                if (s[i_s] != t[i_t]) {
+                    return false;
+                }
+            } else {
+                if (i_s >= 0 || i_t >= 0) {
+                    return false;
+                }
+            }
+            i_s--;
+            i_t--;
+        }
+        return true;
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    string s1 = "a#c";
+    string s2 = "b";
+    bool res = s.backspaceCompare(s1, s2);
+    cout << res << endl;
+    return 0;
+}
+#endif
