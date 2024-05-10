9db479f2c2ec18f3a39d243288b01b5a9decc78b
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-11 02:48:39 +0800

    459 1st 3ms kmp

---
+++ 459.重复的子字符串.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=459 lang=cpp
+ *
+ * [459] 重复的子字符串
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    inline void getNext(std::string pattern, std::vector<int>& next) {
+        int j = 0;
+        next[0] = j;
+        for (int i = 1; i < pattern.size(); i++) {
+            while (j > 0 && pattern[i] != pattern[j]) {
+                j = next[j - 1];
+            }
+            if (pattern[i] == pattern[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+        vector<int> next(n, 0);
+        getNext(s, next);
+        return n % (n - next[n - 1]) == 0 && next[n - 1] != 0;
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    string str = "abac";
+    bool res = s.repeatedSubstringPattern(str);
+    cout << res << endl;
+    return 0;
+}
+#endif
