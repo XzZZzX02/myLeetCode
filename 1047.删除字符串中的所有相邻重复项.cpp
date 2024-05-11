37426ebbe450056583b2376909a4a560f0439b05
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-11 15:07:09 +0800

    1047 1st 20ms stack

---
+++ 1047.删除字符串中的所有相邻重复项.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1047 lang=cpp
+ *
+ * [1047] 删除字符串中的所有相邻重复项
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+        for (char c : s) {
+            if (res.empty() || res.back() != c) {
+                res += c;
+            } else {
+                res.pop_back();
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    string str = "abbaca";
+    string res = s.removeDuplicates(str);
+    cout << res << endl;
+    return 0;
+}
+#endif
