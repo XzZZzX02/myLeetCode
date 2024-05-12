951cc6a1c6941b239b853ca2b30bd3ef20c51888
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-12 16:47:00 +0800

    71 1st 7ms stack

---
+++ 71.简化路径.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=cpp
+ *
+ * [71] 简化路径
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> paths;
+        string res, s;
+        int i = 1;
+        for (int j = 1; j <= path.length(); j++) {
+            if (path[j] == '/' || j == path.length()) {
+                if (path[j - 1] == '/') {
+                    i++;
+                    continue;
+                }
+                s = path.substr(i, j - i);
+                i = j + 1;
+                if (s == "..") {
+                    if (!paths.empty()) {
+                        paths.pop();
+                    }
+                } else if (s != ".") {
+                    paths.push(s);
+                }
+            }
+        }
+        while (!paths.empty()) {
+            res = "/" + paths.top() + res;
+            paths.pop();
+        }
+        return res.empty() ? "/" : res;
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    string path = "/a//b////c/d//././/..";
+    string res = s.simplifyPath(path);
+    cout << res << endl;
+    return 0;
+}
+#endif
