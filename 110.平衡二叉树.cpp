6b2b4a8cb327ee434db8a135263f15db7c044273
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-12 23:58:59 +0800

    110 1st 3ms

---
+++ 110.平衡二叉树.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=cpp
+ *
+ * [110] 平衡二叉树
+ */
+#include "leetcode.h"
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode *root) {
+        if (!root) return 0;
+        int left_height = height(root->left);
+        int right_height = height(root->right);
+        return 1 + max(left_height, right_height);
+    }
+    bool isBalanced(TreeNode *root) {
+        if (!root) return true;
+        int left_height = height(root->left);
+        int right_height = height(root->right);
+        if ((left_height - right_height) * (left_height - right_height) > 1)
+            return false;
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
+// @lc code=end
