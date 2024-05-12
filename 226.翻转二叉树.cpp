ff0fefb43549c903a8bfebce38cee73d93319a3b
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-12 17:17:33 +0800

    226 1st 0ms

---
+++ 226.翻转二叉树.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=cpp
+ *
+ * [226] 翻转二叉树
+ */
+#include "leetcode.h"
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *invertTree(TreeNode *root) {
+        if (!root) return nullptr;
+        TreeNode *res = new TreeNode(root->val);
+        res->left = invertTree(root->right);
+        res->right = invertTree(root->left);
+        return res;
+    }
+};
+// @lc code=end
