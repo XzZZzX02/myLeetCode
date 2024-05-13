b3948dd85f2a21b2eeb6ad4d150ef7026f37694d
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-14 00:08:34 +0800

    404 1st 0ms

---
+++ 404.左叶子之和.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=cpp
+ *
+ * [404] 左叶子之和
+ */
+#include "leetcode.h"
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode *root) {
+        if (!root) return 0;
+        int res = 0;
+        if (root->left && !root->left->left && !root->left->right) {
+            res += root->left->val;
+        }
+        return res + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    TreeNode *root = new TreeNode(
+        1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+    int res = s.sumOfLeftLeaves(root);
+    cout << res << endl;
+    return 0;
+}
+#endif
