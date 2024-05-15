0dbdb00ff6a6a4a3f6e3e3249e6ebd73b156e72e
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-15 10:20:02 +0800

    98 1st 7ms

---
+++ 98.验证二叉搜索树.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=cpp
+ *
+ * [98] 验证二叉搜索树
+ */
+#include "leetcode.h"
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode *root, long minVal = LONG_MIN,
+                    long maxVal = LONG_MAX) {
+        if (!root) return true;
+        if (root->left &&
+            (root->left->val >= root->val || root->left->val <= minVal)) {
+            return false;
+        }
+        if (root->right &&
+            (root->right->val <= root->val || root->right->val >= maxVal)) {
+            return false;
+        }
+        return isValidBST(root->left, minVal, root->val) &&
+               isValidBST(root->right, root->val, maxVal);
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    TreeNode *root = new TreeNode(-2147483648);
+    root->right = new TreeNode(2147483647);
+    bool res = s.isValidBST(root);
+    cout << res << endl;
+    return 0;
+}
+#endif
