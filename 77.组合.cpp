68d16b7f7e3896befe850825a14bbb8ed95c65f7
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-21 22:26:15 +0800

    77 1st 77ms

---
+++ 77.组合.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=cpp
+ *
+ * [77] 组合
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    void backtracing(vector<int>& num, vector<vector<int>>& re, int k,
+                     int start, int max) {
+        int n = num.size();
+        int u = 0;
+        if (n == k) {
+            re.push_back(num);
+            return;
+        }
+        for (int i = start; i <= max - k + n + 1; i++) {
+            num.push_back(i);
+            backtracing(num, re, k, i + 1, max);
+            num.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> nums;
+        vector<vector<int>> res;
+        backtracing(nums, res, k, 1, n);
+        return res;
+    }
+};
+// @lc code=end
