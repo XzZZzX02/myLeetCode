5534e602d25c1779396dd61aa96dc714abbe9176
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-21 22:27:32 +0800

    216 1st 0ms backtracing+pruning

---
+++ 216.组合总和-iii.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=216 lang=cpp
+ *
+ * [216] 组合总和 III
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    void backtracing(vector<int>& num, vector<vector<int>>& res, int start,
+                     int k, int nu, int sum, int target) {
+        int n = num.size();
+
+        if (n == k) {
+            if (sum == target) {
+                res.push_back(num);
+            }
+            return;
+        }
+
+        for (int i = start; i <= nu - k + n + 1; i++) {
+            num.push_back(i);
+            backtracing(num, res, i + 1, k, nu, sum + i, target);
+            num.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> res;
+        vector<int> num;
+        backtracing(num, res, 1, k, 9, 0, n);
+        return res;
+    }
+};
+// @lc code=end
