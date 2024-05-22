84f6694395a1511bedc546f9418a39795de404ef
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-23 01:36:47 +0800

    archive backtrack

--- backtrack/17.电话号码的字母组合.cpp
+++ backtrack/17.电话号码的字母组合.cpp
@@ -10,13 +10,8 @@ class Solution {
                         "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
 public:
-<<<<<<< HEAD
-    void backtracing(vector<string>& res, string s, string digits, int start,
-                     int n) {
-=======
     void backtracking(vector<string>& res, string s, string digits, int start,
                       int n) {
->>>>>>> 675adc4 (17 1st 0ms backtracking)
         if (start == n) {
             if (!s.empty()) {
                 res.push_back(s);
@@ -26,11 +21,7 @@ public:
         string le = m[digits[start] - '0'];
         for (char l : le) {
             s.push_back(l);
-<<<<<<< HEAD
-            backtracing(res, s, digits, start + 1, n);
-=======
             backtracking(res, s, digits, start + 1, n);
->>>>>>> 675adc4 (17 1st 0ms backtracking)
             s.pop_back();
         }
     }
@@ -38,11 +29,7 @@ public:
         vector<string> res;
         int n = digits.size();
         string t = "";
-<<<<<<< HEAD
-        backtracing(res, t, digits, 0, n);
-=======
         backtracking(res, t, digits, 0, n);
->>>>>>> 675adc4 (17 1st 0ms backtracking)
         return res;
     }
 };
