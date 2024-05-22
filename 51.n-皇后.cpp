0331ff75e28fc31255be44b0d2e18941c1fea022
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-23 00:56:43 +0800

    51 1st 3ms backtracking

---
+++ 51.n-皇后.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=cpp
+ *
+ * [51] N 皇后
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+    vector<vector<string>> res;
+
+public:
+    bool isValid(int row, int col, int n, vector<string>& chessboard) {
+        for (int i = 0; i < row; i++) {
+            if (chessboard[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chessboard[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (chessboard[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    void backtracking(int row, int n, vector<string>& chessboard) {
+        if (row == n) {
+            res.push_back(chessboard);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isValid(row, col, n, chessboard)) {
+                chessboard[row][col] = 'Q';
+                backtracking(row + 1, n, chessboard);
+                chessboard[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        res.clear();
+        vector<string> chessboard(n, string(n, '.'));
+        backtracking(0, n, chessboard);
+        return res;
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    int n = 4;
+    vector<vector<string>> res = s.solveNQueens(n);
+    for (auto v : res) {
+        for (auto s : v) {
+            cout << s << endl;
+        }
+        cout << "=================" << endl;
+    }
+    return 0;
+}
+#endif
