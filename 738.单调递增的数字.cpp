72322533776d56edbc5d773a8d50242ab11e095e
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-25 23:38:17 +0800

    738 1st 0ms greedy

---
+++ 738.单调递增的数字.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=738 lang=cpp
+ *
+ * [738] 单调递增的数字
+ */
+#include <string>
+
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+    vector<int> bits;
+
+public:
+    int monotoneIncreasingDigits(int n) {
+        string str = to_string(n);
+        int flag = str.size();
+        for (int i = str.size() - 1; i > 0; i--) {
+            if (str[i - 1] > str[i]) {
+                flag = i;
+                str[i - 1]--;
+            }
+        }
+        for (int i = flag; i < str.size(); i++) {
+            str[i] = '9';
+        }
+        return stoi(str);
+    }
+};
+// @lc code=end
