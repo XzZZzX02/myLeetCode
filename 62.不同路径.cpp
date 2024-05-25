ec0fd397a8e11829d6c2b27f058c6239bd44894f
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-26 01:37:13 +0800

    62 2st TLE dfs

--- 62.不同路径.cpp
+++ 62.不同路径.cpp
@@ -7,16 +7,16 @@
 // @lc code=start
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        int res = 0;
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
+    int dfs(int m, int n, int i, int j) {
+        if (i > m || j > n) {
+            return 0;
         }
-        return dp[m - 1][n - 1];
+        if (i == m && j == n) {
+            return 1;
+        }
+        return dfs(m, n, i + 1, j) + dfs(m, n, i, j + 1);
     }
+    int uniquePaths(int m, int n) { return dfs(m, n, 1, 1); }
 };
 // @lc code=end
 #ifndef __ONLINE_JUDGE__
