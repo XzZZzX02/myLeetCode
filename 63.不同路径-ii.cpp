627736348dcc1f31a91275862aeaf35655269ebb
coder-Zzx <zzzzzx235711@gmail.com>
2024-05-26 02:26:30 +0800

    63 1st 0ms dp

---
+++ 63.不同路径-ii.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ *
+ * [63] 不同路径 II
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid.back().back() == 1 || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        vector<vector<int>> dp(obstacleGrid.size(),
+                               vector<int>(obstacleGrid[0].size(), 0));
+        for (int i = 0; i < obstacleGrid.size() && !obstacleGrid[i][0]; i++) {
+            dp[i][0] = obstacleGrid[i][0] == 0;
+        }
+        for (int j = 0; j < obstacleGrid[0].size() && !obstacleGrid[0][j];
+             j++) {
+            dp[0][j] = obstacleGrid[0][j] == 0;
+        }
+        for (int i = 1; i < obstacleGrid.size(); i++) {
+            for (int j = 1; j < obstacleGrid[0].size(); j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[obstacleGrid.size() - 1][obstacleGrid[0].size() - 1];
+    }
+};
+// @lc code=end
+#ifndef __ONLINE_JUDGE__
+int main() {
+    Solution s;
+    vector<vector<int>> obs = {{0, 0}, {1, 1}, {0, 0}};
+    int res = s.uniquePathsWithObstacles(obs);
+    cout << res << endl;
+    return 0;
+}
+#endif
