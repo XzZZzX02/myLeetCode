5b87c78d0dbc7e811dc81efc661ad23ada4ef48a
coder-Zzx <zzzzzx235711@gmail.com>
2024-06-03 23:32:33 +0800

    518 1st 0ms dp

---
+++ 518.零钱兑换-ii.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < coins.size(); i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
+// @lc code=end
