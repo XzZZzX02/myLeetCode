d570197703e95427c3864af8e47512600899fae6
coder-Zzx <zzzzzx235711@gmail.com>
2024-06-04 01:54:41 +0800

    139 2nd 4ms dp

--- 139.单词拆分.cpp
+++ 139.单词拆分.cpp
@@ -7,28 +7,19 @@
 // @lc code=start
 class Solution {
 public:
-    bool backtracking(const string& s, const unordered_set<string>& wordSet,
-                      vector<bool>& used, int start) {
-        if (start >= s.size()) {
-            return true;
-        }
-        if (!used[start]) {
-            return used[start];
-        }
-        for (int i = start; i < s.size(); i++) {
-            string word = s.substr(start, i - start + 1);
-            if (wordSet.find(word) != wordSet.end() &&
-                backtracking(s, wordSet, used, i + 1)) {
-                return true;
-            }
-        }
-        used[start] = false;
-        return false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
-        vector<bool> used(s.size(), true);
-        return backtracking(s, wordSet, used, 0);
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                string sub = s.substr(j, i - j);
+                if (dp[j] && wordSet.count(sub)) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.size()];
     }
 };
 // @lc code=end
