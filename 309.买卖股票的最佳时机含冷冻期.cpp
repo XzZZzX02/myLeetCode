1325da5f44117bd85a6b49c7e13b29934f44bc5a
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-13 14:34:56 +0000

    309 1st 0ms dp

---
+++ 309.买卖股票的最佳时机含冷冻期.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=309 lang=cpp
+ *
+ * [309] 买卖股票的最佳时机含冷冻期
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (!n) {
+            return 0;
+        }
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] -= prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][3] - prices[i], dp[i - 1][1] - prices[i]));
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
+            dp[i][3] = dp[i - 1][2];
+        }
+        return max(dp[n - 1][3], max(dp[n - 1][1], dp[n - 1][2]));
+    }
+};
+// @lc code=end
+
+
