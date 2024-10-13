ce907b8573490eac7b33b7b9fd2498a262f5cf1f
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-13 15:05:10 +0000

    714 3rd 32ms dp

--- 714.买卖股票的最佳时机含手续费.cpp
+++ 714.买卖股票的最佳时机含手续费.cpp
@@ -4,25 +4,21 @@
  * [714] 买卖股票的最佳时机含手续费
  */
 #include "leetcode.h"
+#define v vector
+#define _rep(i,a,b) for (int i=(a);i<(b);++i)
 // @lc code=start
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int res = 0, n = prices.size();
-        int minPrice = prices[0];
-        for (int i = 0; i < n; i++) {
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            }
-            if (prices[i] >= minPrice && prices[i] <= minPrice + fee) {
-                continue;
-            }
-            if (prices[i] > minPrice + fee) {
-                res += prices[i] - minPrice - fee;
-                minPrice = prices[i] - fee;
-            }
+        ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        int n = prices.size(), dp0 = 0, dp1;
+        dp1 = INT_MIN;
+        for (int i = 1;i < n + 1;++i) {
+            int dp2 = max(dp0, dp1 + prices[i - 1]);
+            dp1 = max(dp1, dp0 - prices[i - 1] - fee);
+            dp0 = dp2;
         }
-        return res;
+        return dp0;
     }
 };
 // @lc code=end
