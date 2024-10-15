8ac6483e88e34706959e35e35d68c34f741c2ca0
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-15 16:53:32 +0000

    392 2nd 0ms dp

--- 392.判断子序列.cpp
+++ 392.判断子序列.cpp
@@ -8,14 +8,17 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i = 0, j = 0;
-        while (i < s.size() && j < t.size()) {
-            if (s[i] == t[j]) {
-                i++;
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= t.size(); j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
             }
-            j++;
         }
-        return i == s.size();
+        return dp[s.size()][t.size()] == s.size();
     }
 };
 // @lc code=end
