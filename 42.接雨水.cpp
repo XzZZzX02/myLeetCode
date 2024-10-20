7785e62e26c145a72d2de0594360f6e870afeede
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-20 16:55:01 +0000

    42 2nd 0ms double pointers

--- 42.接雨水.cpp
+++ 42.接雨水.cpp
@@ -12,19 +12,22 @@ public:
             return 0;
         }
         int res = 0;
-        for (int i = 1; i < height.size() - 1; i++) {
-            int lHeight = height[i], rHeight = height[i];
-            for (int l = i - 1; l >= 0; l--) {
-                if (height[l] > lHeight) {
-                    lHeight = height[l];
-                }
+        vector<int> maxLeft(height.size(), 0);
+        vector<int> maxRight(height.size(), 0);
+        int n = height.size();
+        maxLeft[0] = height[0];
+        maxRight[n - 1] = height[n - 1];
+        for (int i = 1; i < n; i++) {
+            maxLeft[i] = max(height[i], maxLeft[i - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            maxRight[i] = max(height[i], maxRight[i + 1]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            int count = min(maxLeft[i], maxRight[i]) - height[i];
+            if (count > 0) {
+                res += count;
             }
-            for (int r = i + 1; r < height.size(); r++) {
-                if (height[r] > rHeight) {
-                    rHeight = height[r];
-                }
-            }
-            res += min(lHeight, rHeight) - height[i];
         }
         return res;
     }
