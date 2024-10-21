403a56d107ec3a84d4246b3e9e8b07cef387c957
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-21 07:38:01 +0000

    84 2nd 8ms double pointers

--- 84.柱状图中最大的矩形.cpp
+++ 84.柱状图中最大的矩形.cpp
@@ -9,19 +9,26 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int res = 0;
-        for (int i = 0; i < heights.size(); i++) {
-            int l = i, r = i;
-            for (;l >= 0; l--) {
-                if (heights[l] < heights[i]) {
-                    break;
-                }
+        int n = heights.size();
+        vector<int> minLeft(heights.size()), minRight(heights.size());
+        minLeft[0] = -1;
+        for (int i = 1; i < n; i++) {
+            int t = i - 1;
+            while (t >= 0 && heights[t] >= heights[i]) {
+                t = minLeft[t];
             }
-            for (; r < heights.size(); r++) {
-                if (heights[r] < heights[i]) {
-                    break;
-                }
+            minLeft[i] = t;
+        }
+        minRight[n - 1] = n;
+        for (int i = n - 2; i >= 0; i--) {
+            int t = i + 1;
+            while (t < n && heights[t] >= heights[i]) {
+                t = minRight[t];
             }
-            res = max(res, (r - l - 1) * heights[i]);
+            minRight[i] = t;
+        }
+        for (int i = 0; i < n; i++) {
+            res = max(res, heights[i] * (minRight[i] - minLeft[i] - 1));
         }
         return res;
     }
