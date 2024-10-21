be3e9d091d9c53554408cc10cc277801c9364808
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-21 07:44:22 +0000

    84 3rd 3ms monotonic stack

--- 84.柱状图中最大的矩形.cpp
+++ 84.柱状图中最大的矩形.cpp
@@ -8,27 +8,18 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int res = 0;
+        heights.push_back(0);
         int n = heights.size();
-        vector<int> minLeft(heights.size()), minRight(heights.size());
-        minLeft[0] = -1;
-        for (int i = 1; i < n; i++) {
-            int t = i - 1;
-            while (t >= 0 && heights[t] >= heights[i]) {
-                t = minLeft[t];
-            }
-            minLeft[i] = t;
-        }
-        minRight[n - 1] = n;
-        for (int i = n - 2; i >= 0; i--) {
-            int t = i + 1;
-            while (t < n && heights[t] >= heights[i]) {
-                t = minRight[t];
-            }
-            minRight[i] = t;
-        }
+        vector<int> vec;
+        int res = 0;
         for (int i = 0; i < n; i++) {
-            res = max(res, heights[i] * (minRight[i] - minLeft[i] - 1));
+            while (!vec.empty() && heights[vec.back()] > heights[i]) {
+                int out = vec.back();
+                vec.pop_back();
+                res = max(res, heights[out] *
+                                   (vec.empty() ? i : i - (vec.back() + 1)));
+            }
+            vec.push_back(i);
         }
         return res;
     }
