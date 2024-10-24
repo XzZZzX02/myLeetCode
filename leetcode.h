b239687421f801758a295dd245e413e78cfae0d6
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-24 15:29:08 +0000

    133 1st 0ms dfs

--- leetcode.h
+++ leetcode.h
@@ -13,3 +13,21 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right)
         : val(x), left(left), right(right) {}
 };
+
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
