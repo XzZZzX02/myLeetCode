5f225f806d6938f148a0a5c6f8d5928dd28184be
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-10-26 13:08:45 +0000

    3181 1st 630ms dp+bit

---
+++ 3181.执行操作可获得的最大总奖励-ii.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=3181 lang=cpp
+ *
+ * [3181] 执行操作可获得的最大总奖励 II
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rewardValues) {
+        if (rewardValues.empty()) {
+            return 0;
+        }
+        sort(rewardValues.begin(), rewardValues.end());
+        rewardValues.erase(unique(rewardValues.begin(), rewardValues.end()), rewardValues.end());
+        bitset<100000> dp{1};
+        for (int v : rewardValues) {
+            int shift = dp.size() - v;
+            dp |= dp << shift >> (shift - v);
+        }
+        for (int i = rewardValues.back() * 2 - 1;; i--) {
+            if (dp.test(i)) {
+                return i;
+            }
+        }
+    }
+};
+// @lc code=end
+
