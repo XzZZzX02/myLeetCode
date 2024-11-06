7b5617f5fb69c4d0635bd924a05dfbc85d0ff467
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-06 16:08:42 +0000

    802 2nd 54ms topological sort

--- 802.找到最终的安全状态.cpp
+++ 802.找到最终的安全状态.cpp
@@ -10,28 +10,33 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         vector<int> res;
         int n = graph.size();
-        vector<int> color(n, 0);
+        vector<int> indeg(n);
+        vector<vector<int>> rg(n);
+        queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            for (int j : graph[i]) {
+                rg[j].push_back(i);
+            }
+            indeg[i] = graph[i].size();
+            if (indeg[i] == 0) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop();
+            for (int j : rg[i]) {
+                if (--indeg[j] == 0) {
+                    q.push(j);
+                }
+            }
+        }
         for (int i = 0; i < n; i++) {
-            if (dfs(i, color, graph)) {
+            if (indeg[i] == 0) {
                 res.push_back(i);
             }
         }
         return res;
     }
-
-    bool dfs(int x, vector<int>& color, vector<vector<int>>& graph) {
-        if (color[x]) {
-            return color[x] == 2;
-        }
-        color[x] = 1;
-        for (auto v : graph[x]) {
-            if (!dfs(v, color, graph)) {
-                return false;
-            }
-        }
-        color[x] = 2;
-        return true;
-    }
 };
 // @lc code=end
-
