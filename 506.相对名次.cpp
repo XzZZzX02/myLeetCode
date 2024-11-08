656ee82c1965b0f58357083330ad8f3e5e56a1ff
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-08 16:11:14 +0000

    506 2nd 0ms dp

--- 506.相对名次.cpp
+++ 506.相对名次.cpp
@@ -9,23 +9,18 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         vector<string> res(score.size());
-        vector<int> rank(score.size());
-        rank = score;
-        sort(rank.begin(), rank.end(), greater<int>());
-        for (int i = 0; i < score.size(); i++) {
-            for (int j = 0; j < rank.size(); j++) {
-                if (score[i] == rank[j]) {
-                    if (j == 0) {
-                        res[i] = "Gold Medal";
-                    } else if (j == 1) {
-                        res[i] = "Silver Medal";
-                    } else if (j == 2) {
-                        res[i] = "Bronze Medal";
-                    } else {
-                        res[i] = to_string(j + 1);
-                    }
-                    break;
-                }
+        vector<pair<int, int>> arr;
+        vector<string> medal = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        int n = score.size();
+        for (int i = 0; i < n; i++) {
+            arr.push_back({-score[i], i});
+        }
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < n; i++) {
+            if (i < 3) {
+                res[arr[i].second] = medal[i];
+            } else {
+                res[arr[i].second] = to_string(i + 1);
             }
         }
         return res;
