6c2a97bb33a80f4c4c4cc20d714949547340cc13
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-15 17:32:57 +0000

    220 1st 40ms 桶排序+滑动窗口

--- 220.存在重复元素-iii.cpp
+++ 220.存在重复元素-iii.cpp
@@ -3,25 +3,37 @@
  *
  * [220] 存在重复元素 III
  */
-#include <vector>
-
 #include "leetcode.h"
 // @lc code=start
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff,
                                        int valueDiff) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i - 1; j >= 0 && i - j <= indexDiff; j--) {
-                if (abs((long long)nums[i] - (long long)nums[j]) <= valueDiff) {
-                    return true;
+        int l = 0, r = 0, n = nums.size(), maxNum = nums[0], minNum = nums[0];
+        for (int i = 0; i < n; i++) {
+            maxNum = max(maxNum, nums[i]);
+            minNum = min(minNum, nums[i]);
+        }
+        vector<int> buckets((maxNum - minNum) / (valueDiff + 1) + 3, 0);
+        while (r < n) {
+            int temp = (nums[r] - minNum) / (valueDiff + 1) + 1;
+            buckets[temp]++;
+            if (buckets[temp] > 1) {
+                return true;
+                break;
+            } else if (valueDiff > 0 &&
+                       (buckets[temp - 1] > 0 || buckets[temp + 1] > 0)) {
+                for (int i = l; i < r; i++) {
+                    if (abs(nums[r] - nums[i]) <= valueDiff) {
+                        return true;
+                        break;
+                    }
                 }
             }
-            for (int j = i + 1; j < nums.size() && j - i <= indexDiff; j++) {
-                if (abs((long long)nums[i] - (long long)nums[j]) <= valueDiff) {
-                    return true;
-                }
+            if (r - l >= indexDiff) {
+                buckets[(nums[l++] - minNum) / (valueDiff + 1) + 1]--;
             }
+            r++;
         }
         return false;
     }
