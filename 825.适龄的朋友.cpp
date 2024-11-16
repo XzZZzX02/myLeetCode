a88757008b9c9e97b8cd96c9f7b597667bb628c2
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-16 17:18:18 +0000

    825 1st 0ms 双指针

---
+++ 825.适龄的朋友.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=825 lang=cpp
+ *
+ * [825] 适龄的朋友
+ */
+#include <algorithm>
+
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        int res = 0;
+        sort(ages.begin(), ages.end());
+        
+        for (int k = 0, i = 0, j = 0; k < ages.size(); k++) {
+            while (i < k && !check(ages[i], ages[k])) {
+                i++;
+            }
+            if (j < k) {
+                j = k;
+            }
+            while (j < ages.size() && check(ages[j], ages[k])) {
+                j++;
+            }
+            if (j > i) {
+                res += j - i - 1;
+            }
+        }
+        return res;
+    }
+    bool check(int x, int y) {
+        if (y <= 0.5 * x + 7) {
+            return false;
+        }
+        if (y > x) {
+            return false;
+        }
+        if (y > 100 && x < 100) {
+            return false;
+        }
+        return true;
+    }
+};
+// @lc code=end
