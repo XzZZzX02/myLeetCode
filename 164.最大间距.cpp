9489cac48bc3a339009d791ea5c729efad346383
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-18 14:11:13 +0000

    164 2nd 7ms 基数排序

--- 164.最大间距.cpp
+++ 164.最大间距.cpp
@@ -11,13 +11,29 @@ public:
         if (nums.size() < 2) {
             return 0;
         }
-        int res = 0;
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); i++) {
-            res = max(res, nums[i] - nums[i - 1]);
+        int res = 0, n = nums.size(), prev = INT_MAX;
+        vector<vector<int>> bucket(10, vector<int>());
+        int maxNum = INT_MIN, minNum = INT_MAX;
+        for (auto& nums : nums) {
+            maxNum = max(maxNum, nums);
+            minNum = min(minNum, nums);
+        }
+        int bucketSize = max(1, (maxNum - minNum) / (n - 1));
+        int bucketNum = (maxNum - minNum) / bucketSize + 1;
+        vector<pair<int, int>> buckets(bucketNum, {INT_MAX, INT_MIN});
+        for (auto& num : nums) {
+            int i = (num - minNum) / bucketSize;
+            buckets[i].first = min(buckets[i].first, num);
+            buckets[i].second = max(buckets[i].second, num);
+        }
+        for (auto& [curMin, curMax] : buckets) {
+            if (curMin > curMax) {
+                continue;
+            }
+            res = max(res, curMin - prev);
+            prev = curMax;
         }
         return res;
     }
 };
 // @lc code=end
-
