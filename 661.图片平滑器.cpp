a5a6e9b92bfc666dbf9993c9e68a36aa6cc2f6bc
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-18 02:18:43 +0000

    661 1st 0ms

---
+++ 661.图片平滑器.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=661 lang=cpp
+ *
+ * [661] 图片平滑器
+ */
+#include "leetcode.h"
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size(), n = img[0].size();
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int s = 0, cnt = 0;
+                for (int x = i - 1; x <= i + 1; ++x) {
+                    for (int y = j - 1; y <= j + 1; ++y) {
+                        if (x < 0 || x >= m || y < 0 || y >= n) continue;
+                        ++cnt;
+                        s += img[x][y];
+                    }
+                }
+                res[i][j] = s / cnt;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
