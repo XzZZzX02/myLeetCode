2b307d289240ad3c15ce36924c07e008c9a5cc01
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-19 12:22:13 +0000

    153 2nd 0ms 二分查找

--- 153.寻找旋转排序数组中的最小值.cpp
+++ 153.寻找旋转排序数组中的最小值.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        return *min_element(nums.begin(), nums.end());
+        int n = nums.size();
+        if (nums[0] <= nums[n - 1]) {
+            return nums[0];
+        }
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
     }
 };
 // @lc code=end
