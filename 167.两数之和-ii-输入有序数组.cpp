8d7b01c7f2a0150efe54b68377f19019e3da51b5
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-19 12:13:57 +0000

    167 2nd 0ms 双指针

--- 167.两数之和-ii-输入有序数组.cpp
+++ 167.两数之和-ii-输入有序数组.cpp
@@ -8,11 +8,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        for (int i = 0; i < numbers.size(); i++) {
-            for (int j = i + 1; j < numbers.size(); j++) {
-                if (numbers[i] + numbers[j] == target && i != j) {
-                    return {i + 1, j + 1};
-                }
+        int left = 0, right = numbers.size() - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return {left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
         return {-1, -1};
