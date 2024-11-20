082c7cef3f8479586121bfe10de3e71eee831850
Zhixin Zhu <zzzzzx235711@gmail.com>
2024-11-20 11:51:24 +0000

    81 2nd 0ms 二分查找

--- 81.搜索旋转排序数组-ii.cpp
+++ 81.搜索旋转排序数组-ii.cpp
@@ -8,20 +8,32 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
         int left = 0, right = nums.size() - 1;
-        while (left <= right) {
-            int mid = (right - left) / 2 + left;
+        while (left < right) {
+            int mid = (left + right) >> 1;
             if (nums[mid] == target) {
                 return true;
             }
-            if (nums[mid] < target) {
-                left = mid + 1;
+            if (nums[left] < nums[mid]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[left] > nums[mid]) {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid;
+                } else {
+                    right = mid - 1;
+                }
             } else {
-                right = mid - 1;
+                if (nums[left] == target) {
+                    return true;
+                }
+                left++;
             }
         }
-        return false;
+        return nums[left] == target;
     }
 };
 // @lc code=end
